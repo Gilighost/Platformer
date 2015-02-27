4052e9cdfc460cef09a293d29d709b39aeddb740
nkroberts <nroberts@harding.edu>
2015-02-26 18:30:47 -0600

    world and simple camera

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -1,62 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace Platformer
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _batch;
+        private KeyboardState _oldKeyState;
+        private GamePadState _oldPadState;
+
+        private World _world;
+
+        // Simple camera controls
+        private Matrix _view;
+        private Vector2 _cameraPosition;
+        private Vector2 _screenCenter;
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 480;
+
             Content.RootDirectory = "Content";
+
+            //Create a world with gravity.
+            _world = new World(new Vector2(0, 9.82f));
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Initialize camera controls
+            _view = Matrix.Identity;
+            _cameraPosition = Vector2.Zero;
+            _screenCenter = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2f, _graphics.GraphicsDevice.Viewport.Height / 2f);
+            _batch = new SpriteBatch(_graphics.GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
         }
 
         /// <summary>
@@ -66,13 +50,52 @@ namespace Platformer
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
+            HandleGamePad();
+            HandleKeyboard();
 
-            // TODO: Add your update logic here
+            //We update the world
+            _world.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
 
             base.Update(gameTime);
+        }
+
+        private void HandleGamePad()
+        {
+            GamePadState padState = GamePad.GetState(0);
+
+            if (padState.IsConnected)
+            {
+                if (padState.Buttons.Back == ButtonState.Pressed)
+                    Exit();
+
+                _oldPadState = padState;
+            }
+        }
+
+        private void HandleKeyboard()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            // Move camera
+
+            if (state.IsKeyDown(Keys.Left))
+                _cameraPosition.X += 1.5f;
+
+            if (state.IsKeyDown(Keys.Right))
+                _cameraPosition.X -= 1.5f;
+
+            if (state.IsKeyDown(Keys.Up))
+                _cameraPosition.Y += 1.5f;
+
+            if (state.IsKeyDown(Keys.Down))
+                _cameraPosition.Y -= 1.5f;
+
+            _view = Matrix.CreateTranslation(new Vector3(_cameraPosition - _screenCenter, 0f)) * Matrix.CreateTranslation(new Vector3(_screenCenter, 0f));
+
+            if (state.IsKeyDown(Keys.Escape))
+                Exit();
+
+            _oldKeyState = state;
         }
 
         /// <summary>
@@ -83,7 +106,10 @@ namespace Platformer
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            //Draw circle and ground
+            _batch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, _view);
+
+            _batch.End();
 
             base.Draw(gameTime);
         }
