9b8efef59e80d7a215672b9d1f32b9e2dad00743
nkroberts <nroberts@harding.edu>
2015-03-19 13:45:46 -0500

    Improved color transitions

--- Platformer/Platformer/Block.cs
+++ Platformer/Platformer/Block.cs
@@ -16,12 +16,11 @@ namespace Platformer
 {
     class Block : Component
     {
-        private int timer, rInc, gInc, bInc;
+        private int colorInc;
 
         public Block(Vector2 coordinates)
         {
             Position = ConvertUnits.ToSimUnits(new Vector2(coordinates.X * 64, coordinates.Y * 64));
-            timer = random.Next(0, 601);
         }
 
         public override void BuildComponent(World world, Texture2D texture)
@@ -35,8 +34,6 @@ namespace Platformer
             Body.Friction = 1f;
 
             Origin = new Vector2(Texture.Width / 2, Texture.Height / 2);
-            
-            int red = random.Next(0, 256), green = random.Next(0, 256), blue = random.Next(0, 256);
 
             GenerateColor();
         }
@@ -90,6 +87,16 @@ namespace Platformer
 
             Color = new Color(red, green, blue);
 
+            i = random.Next(1, 3);
+
+            if (i == 1)
+            {
+                colorInc = random.Next(1, 4);
+            }
+            else
+            {
+                colorInc = random.Next(-3, 0);
+            }
         }
 
         public override void Update(VisualizationData visData)
@@ -97,54 +104,123 @@ namespace Platformer
             //change component data based on music visualization input
             //GenerateColor(); //WARNING EPILEPSY
 
-            if (timer > 0)
+
+
+            int red = (int)Color.R, green = (int)Color.G, blue = (int)Color.B;
+
+            if (red > 0 && red < 255)
             {
-                timer--;
+                if (red + colorInc >= 0)
+                {
+                    if (red + colorInc <= 255)
+                    {
+                        red += colorInc;
+                    }
+                    else
+                    {
+                        red = 255;
+                    }
+                }
+                else
+                {
+                    red = 0;
+                }
+            }
+            else if (green > 0 && green < 255)
+            {
+                if (green + colorInc >= 0)
+                {
+                    if (green + colorInc <= 255)
+                    {
+                        green += colorInc;
+                    }
+                    else
+                    {
+                        green = 255;
+                    }
+                }
+                else
+                {
+                    green = 0;
+                }
+            }
+            else if (blue > 0 && blue < 255)
+            {
+                if (blue + colorInc >= 0)
+                {
+                    if (blue + colorInc <= 255)
+                    {
+                        blue += colorInc;
+                    }
+                    else
+                    {
+                        blue = 255;
+                    }
+                }
+                else
+                {
+                    blue = 0;
+                }
             }
             else
             {
+                int i;
+                bool newTransitionChosen = false; ;
 
-                int red = (int)Color.R, green = (int)Color.G, blue = (int)Color.B;
+                while (!newTransitionChosen)
+                {
+                    i = random.Next(1,4);
 
-                if (red <= 0)
-                {
-                    red = 0;
-                    rInc = random.Next(1,4);
+                    if (i == 1)
+                    {
+                        if (red == 0 && (green == 0 || blue == 0))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(1, 4);
+                            red += colorInc;
+                        }
+                        else if (red == 255 && (green == 255 || blue == 255))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(-3, 0);
+                            red += colorInc;
+                        }
+                    }
+                    else if (i == 2)
+                    {
+                        if (green == 0 && (red == 0 || blue == 0))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(1, 4);
+                            green += colorInc;
+                        }
+                        else if (green == 255 && (red == 255 || blue == 255))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(-3, 0);
+                            green += colorInc;
+                        }
+                    }
+                    else if (i == 3)
+                    {
+                        if (blue == 0 && (green == 0 || red == 0))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(1, 4);
+                            blue += colorInc;
+                        }
+                        else if (blue == 255 && (green == 255 || red == 255))
+                        {
+                            newTransitionChosen = true;
+                            colorInc = random.Next(-3, 0);
+                            blue += colorInc;
+                        }
+                    }
                 }
-                else if (red >= 255)
-                {
-                    red = 255;
-                    rInc = random.Next(-3, 0);
-                }
-
-                if (green <= 0)
-                {
-                    green = 0;
-                    gInc = random.Next(1, 4);
-                }
-                else if (green >= 255)
-                {
-                    green = 255;
-                    gInc = random.Next(-3, 0);
-                }
-
-                if (blue <= 0)
-                {
-                    blue = 0;
-                    bInc = random.Next(1, 4);
-                }
-                else if (blue >= 255)
-                {
-                    blue = 255;
-                    bInc = random.Next(-3, 0);
-                }
-
-                red += rInc;
-                green += gInc;
-                blue += bInc;
-
-                Color = new Color(red, green, blue);
             }
+
+            Color = new Color(red, green, blue);
+
             base.Update(visData);
         }
 
